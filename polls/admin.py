d281a2c30c19a8fa0bd13e17bf16b870eec50e57
hoang <giahoangth@gmail.com>
2020-12-14 16:16:01 +0700

    copied from mydj

---
+++ polls/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 10
+
+
+# Register your models here.
+# from part2
+# admin.site.register(Question)
+# from part7
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
---
+++ polls/views.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.http.response import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpRequest
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+from .models import Choice, Question
+# Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'lastest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    # Why do we use a helper function get_object
+    # _or_404() instead of automatically
+    #  catching the ObjectDoesNotExist exceptions at a higher level,
+    #  or having the model API raise Http404 instead of ObjectDoesNotExist?
+    # Because that would couple the model layer to the view layer.
+    # Note: There’s also a get_list_or_404() function,
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    question: Question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice: Choice = question.choice_set.get(
+            pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redirect the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    except Exception as e:
+        print(e)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question.id,))
+        )
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def owner(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello, world. 130214d3 is the polls index.")
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
